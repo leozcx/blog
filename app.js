8257f7c47272a2285eef790560ec890f808f9795
Leo <zcx8532@gmail.com>
2015-09-02 20:08:18 +0800

    login

--- app.js
+++ app.js
@@ -4,9 +4,35 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var Strategy = require('passport-local').Strategy;
+
 
 var routes = require('./routes/index');
 var post = require('./routes/post');
+
+passport.use(new Strategy(function(username, password, cb) {
+	console.log("strategry")
+	var user = {
+			id:"1",
+			username:"user1"
+	};
+ cb(null, user);
+}));
+
+passport.serializeUser(function(user, cb) {
+	console.log("serialize: " + user)
+	  cb(null, user.id);
+	});
+
+passport.deserializeUser(function(id, cb) {
+	console.log("desecira")
+		var user = {
+				id:"1",
+				username:"user1"
+		};
+	  cb(null, user);
+	});
 
 var app = express();
 
@@ -22,10 +48,31 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', routes);
 app.use('/posts', post);
 
+
+app.get('/logout',
+		  function(req, res){
+	console.log("logout")
+		    req.logout();
+		    res.redirect('/');
+		  });
+
+
+app.post('/login', 
+		  passport.authenticate('local', { failureRedirect: '/posts' }),
+		  function(req, res) {
+		console.log("login")
+		    res.redirect('/succes');
+		  });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
